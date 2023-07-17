bad1be039d03ea518d87f20664a5034808a22552
Pedro Souza <83237899+iShouldz@users.noreply.github.com>
2023-07-17 20:39:58 -0300

    Atualizando pratica de MySQL

--- seção06/seção03/secao04.sql
+++ seção06/seção03/secao04.sql
@@ -17,6 +17,7 @@ CREATE TABLE produtos(
     
 INSERT INTO tipos_produto (descricao) VALUES ('Computador');
 INSERT INTO tipos_produto (descricao) VALUES ('Impressora');
+INSERT INTO tipos_produto (descricao) VALUES ('Apple');
 
 INSERT INTO produtos(descricao, preco, codigo_tipo) VALUES ('Desktop', '1200', 1);
 INSERT INTO produtos(descricao, preco, codigo_tipo) VALUES ('Laptop', '1800', 1);
@@ -27,16 +28,21 @@ INSERT INTO produtos(descricao, preco, codigo_tipo) VALUES ('Impr. Laser', '500'
 SELECT * FROM tipos_produto;
 SELECT codigo, descricao FROM tipos_produto;
 SELECT codigo, descricao, preco, codigo_tipo FROM produtos;
-
 #Alias
 SELECT p.codigo AS cod, p.descricao AS descri, p.preco AS pre, p.codigo_tipo AS ctp FROM produtos as p;
 
+#DML
+INSERT INTO produtos(descricao, preco, codigo_tipo) VALUES ('Notebook', '1200', 1);
+SELECT * FROM produtos;
+INSERT INTO produtos(descricao, preco, codigo_tipo) VALUES ('Macbook', '1200', 1);
 
+#AO USAR O UPDATE, NUNCA ESQUECER O FILTRO DO WHERE, POIS SEM TEREMOS TODOS OS PRODUTOS SENDO ATUALIZADOS.
+UPDATE produtos set codigo_tipo = 3 WHERE codigo = 6;
+UPDATE produtos set descricao = 'Impressora Laser', preco = '700' WHERE codigo=4;
 
+DELETE FROM produtos WHERE codigo = 4;
 
-
-
-
+ALTER TABLE produtos ADD loja VARCHAR(50) NOT NULL;
 
 
 
