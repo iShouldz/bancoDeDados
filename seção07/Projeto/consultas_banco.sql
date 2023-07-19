6f9a8ea6cf2435b1f51440de79c95d82555a473b
Pedro Souza <83237899+iShouldz@users.noreply.github.com>
2023-07-18 21:46:15 -0300

    Consultas simples no banco

---
+++ seção07/Projeto/consultas_banco.sql
@@ -0,0 +1,20 @@
+#Consultas no banco
+USE farmacia;
+
+-- Consulta simples em tipos produtos
+SELECT * FROM tipo_produto;
+
+-- Consulta simples em fabricantes
+SELECT * FROM fabricante;
+
+-- Consulta simples em medicos
+SELECT * FROM medicos;
+
+-- Consulta simples em compras
+SELECT * FROM compras;
+
+-- Consulta simples em produto compras
+SELECT * FROM produtos_compra;
+
+-- Consulta simples em receita medicas
+SELECT * FROM receitas_medica;
