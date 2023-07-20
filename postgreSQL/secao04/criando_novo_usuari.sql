471a77996f291edcf0e55ef6dca7426da6eca845
Pedro Souza <83237899+iShouldz@users.noreply.github.com>
2023-07-20 16:03:58 -0300

    Criando novo usuario

---
+++ postgreSQL/secao04/criando_novo_usuari.sql
@@ -0,0 +1,16 @@
+-- Criar usuario
+
+-- CREATE USER estagiario WITH PASSWORD '123456';
+
+--CREATE TABLE empresas(
+--	id SERIAL PRIMARY KEY,
+--	nome CHARACTER VARYING(50) NOT NULL,
+	--funcionarios INT NOT NULL,
+	--cidade CHARACTER VARYING(50) NOT NULL
+--);
+
+--Dando privilegios para o estagiario
+-- GRANT ALL ON empresas TO estagiario;
+--Dando permissao de utilziacao
+--GRANT USAGE, SELECT ON SEQUENCE empresas_id_seq TO estagiario;
+
