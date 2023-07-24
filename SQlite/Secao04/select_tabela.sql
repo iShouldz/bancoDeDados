b9ff9881bc571365a3c10c36aace1c3fcdb4cb54
Pedro Souza <83237899+iShouldz@users.noreply.github.com>
2023-07-24 13:44:30 -0300

    alter table

--- SQlite/Secao04/select_tabela.sql
+++ SQlite/Secao04/select_tabela.sql
@@ -3,9 +3,10 @@ SELECT * FROM tipos_produto;
 SELECT * FROM produtos;
 	
 UPDATE produtos SET descricao = 'Acer Aspire', preco = 2500.00 WHERE id = 1;
+--ALTER TABLE produtos ADD data TEXT;
 
 
-SELECT p.id AS 'ID', p.descricao AS 'Descrição do item', p.preco AS 'Valor unitario', t.descricao AS 'Categoria do Item'
+SELECT p.id AS 'ID', p.descricao AS 'Descrição do item', p.preco AS 'Valor unitario', t.descricao AS 'Categoria do Item', p.data AS 'Data de entrada'
 	FROM produtos AS p, tipos_produto AS t
 	WHERE p.codigo_tipo = t.id ORDER BY p.codigo_tipo;
 	
