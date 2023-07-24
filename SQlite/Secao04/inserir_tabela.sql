218640c7f54cab2068c67a770c93d092958ab323
Pedro Souza <83237899+iShouldz@users.noreply.github.com>
2023-07-24 13:27:46 -0300

    inserir na tabela

---
+++ SQlite/Secao04/inserir_tabela.sql
@@ -0,0 +1,8 @@
+INSERT INTO tipos_produto(descricao) VALUES ('Computador');
+INSERT INTO tipos_produto(descricao) VALUES ('Impressora');
+
+INSERT INTO produtos(descricao, preco, codigo_tipo) VALUES ('Desktop', 1200, 1);
+INSERT INTO produtos(descricao, preco, codigo_tipo) VALUES ('Laptop', 1800, 2);
+INSERT INTO produtos(descricao, preco, codigo_tipo) VALUES ('Imp. Jato de Tinta', 300, 2);
+INSERT INTO produtos(descricao, preco, codigo_tipo) VALUES ('Imp. Laser', 500, 1);
+
