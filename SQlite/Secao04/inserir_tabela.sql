275f7582b5c3110a425a5cbbb348fb6c42805268
Pedro Souza <83237899+iShouldz@users.noreply.github.com>
2023-07-24 13:39:55 -0300

    DML - atualização no bd

--- SQlite/Secao04/inserir_tabela.sql
+++ SQlite/Secao04/inserir_tabela.sql
@@ -1,8 +1,9 @@
-INSERT INTO tipos_produto(descricao) VALUES ('Computador');
-INSERT INTO tipos_produto(descricao) VALUES ('Impressora');
-
-INSERT INTO produtos(descricao, preco, codigo_tipo) VALUES ('Desktop', 1200, 1);
-INSERT INTO produtos(descricao, preco, codigo_tipo) VALUES ('Laptop', 1800, 2);
-INSERT INTO produtos(descricao, preco, codigo_tipo) VALUES ('Imp. Jato de Tinta', 300, 2);
-INSERT INTO produtos(descricao, preco, codigo_tipo) VALUES ('Imp. Laser', 500, 1);
-
+-- INSERT INTO tipos_produto(descricao) VALUES ('Computador');
+-- INSERT INTO tipos_produto(descricao) VALUES ('Impressora');
+-- 
+-- INSERT INTO produtos(descricao, preco, codigo_tipo) VALUES ('Desktop', 1200, 1);
+-- INSERT INTO produtos(descricao, preco, codigo_tipo) VALUES ('Laptop', 1800, 2);
+-- INSERT INTO produtos(descricao, preco, codigo_tipo) VALUES ('Imp. Jato de Tinta', 300, 2);
+-- INSERT INTO produtos(descricao, preco, codigo_tipo) VALUES ('Imp. Laser', 500, 1);
+INSERT INTO tipos_produto(descricao) VALUES ('Celulares');
+INSERT INTO produtos(descricao, preco, codigo_tipo) VALUES ('POCO F3', 2600, 3); 
--- SQlite/Secao04/select_tabela.sql
+++ SQlite/Secao04/select_tabela.sql
@@ -1,7 +1,11 @@
 SELECT * FROM tipos_produto;
 
 SELECT * FROM produtos;
+	
+UPDATE produtos SET descricao = 'Acer Aspire', preco = 2500.00 WHERE id = 1;
+
 
 SELECT p.id AS 'ID', p.descricao AS 'Descrição do item', p.preco AS 'Valor unitario', t.descricao AS 'Categoria do Item'
 	FROM produtos AS p, tipos_produto AS t
-	WHERE p.codigo_tipo = t.id;
+	WHERE p.codigo_tipo = t.id ORDER BY p.codigo_tipo;
+	
